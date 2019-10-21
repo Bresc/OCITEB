bd14239fdd03f12c6537e588a86cbb4a4c5b5994
Cristian Guerrero <cristian.guerrero@uptc.edu.co>
2019-10-21 17:42:20 -0500

    fesad funcional

--- frontend/src/app/components/distancia/fac-dis-c01/fac-dis-c01.component.ts
+++ frontend/src/app/components/distancia/fac-dis-c01/fac-dis-c01.component.ts
@@ -13,94 +13,94 @@ import { FacDistanciaC01 } from 'src/app/models/fac-distancia-c01';
 export class FacDisC01Component implements OnInit {
 
   private barChart: am4charts.XYChart;
-  private a12014=0;
-  private a12015=0;
-  private a12016=0;
-  private a12017=0;
-  private a12018=0;
-  private a2014=0;
-  private a2015=0;
-  private a2016=0;
-  private a2017=0;
-  private a2018=0;
-  private b2014=0;
-  private b2015=0;
-  private b2016=0;
-  private b2017=0;
-  private b2018=0;
-  private c2014=0;
-  private c2015=0;
-  private c2016=0;
-  private c2017=0;
-  private c2018=0;
-  private d2014=0;
-  private d2015=0;
-  private d2016=0;
-  private d2017=0;
-  private d2018=0;
-  private r2014=0;
-  private r2015=0;
-  private r2016=0;
-  private r2017=0;
-  private r2018=0;
+  private a12014 = 0;
+  private a12015 = 0;
+  private a12016 = 0;
+  private a12017 = 0;
+  private a12018 = 0;
+  private a2014 = 0;
+  private a2015 = 0;
+  private a2016 = 0;
+  private a2017 = 0;
+  private a2018 = 0;
+  private b2014 = 0;
+  private b2015 = 0;
+  private b2016 = 0;
+  private b2017 = 0;
+  private b2018 = 0;
+  private c2014 = 0;
+  private c2015 = 0;
+  private c2016 = 0;
+  private c2017 = 0;
+  private c2018 = 0;
+  private d2014 = 0;
+  private d2015 = 0;
+  private d2016 = 0;
+  private d2017 = 0;
+  private d2018 = 0;
+  private r2014 = 0;
+  private r2015 = 0;
+  private r2016 = 0;
+  private r2017 = 0;
+  private r2018 = 0;
 
-  constructor(private zone : NgZone, private service: ServiceService) { }
-  
+  constructor(private zone: NgZone, private service: ServiceService) { }
+
   ngOnInit() {
-      this.service.getFacDistanciaC01()
-            .subscribe(res=>{
-                this.service.facDisC01Array = res as FacDistanciaC01[];
-                this.countAportes();
-                this.createChart();
-                
-            });
+    this.service.getFacDistanciaC01()
+      .subscribe(res => {
+        this.service.facDisC01Array = res as FacDistanciaC01[];
+        this.countAportes();
+        this.createChart();
+
+      });
   }
 
-  countAportes(){
+  countAportes() {
     const array = this.service.facDisC01Array as FacDistanciaC01[];
-    for(var _i = 0; _i < array.length; _i++){
-      if(array[_i].ANIO==="2014"){
-         this.a12014=array[_i].A1;
-         this.a2014=array[_i].A;
-         this.b2014=array[_i].B;
-         this.c2014=array[_i].C;
-         this.d2014=array[_i].D;
-         this.r2014=array[_i].R;
-      }else if(array[_i].ANIO=="2015"){
-        this.a12015=array[_i].A1;
-        this.a2015=array[_i].A;
-        this.b2015=array[_i].B;
-        this.c2015=array[_i].C;
-        this.d2015=array[_i].D;
-        this.r2015=array[_i].R;
-      }else if(array[_i].ANIO=="2016"){
-        this.a12016=array[_i].A1;
-        this.a2016=array[_i].A;
-        this.b2016=array[_i].B;
-        this.c2016=array[_i].C;
-        this.d2016=array[_i].D;
-        this.r2016=array[_i].R;
-      }else if(array[_i].ANIO=="2017"){
-        this.a12017=array[_i].A1;
-        this.a2017=array[_i].A;
-        this.b2017=array[_i].B;
-        this.c2017=array[_i].C;
-        this.d2017=array[_i].D;
-        this.r2017=array[_i].R;
-      }else if(array[_i].ANIO=="2018"){
-        this.a12018=array[_i].A1;
-        this.a2018=array[_i].A;
-        this.b2018=array[_i].B;
-        this.c2018=array[_i].C;
-        this.d2018=array[_i].D;
-        this.r2018=array[_i].R;
+    for (var _i = 0; _i < array.length; _i++) {
+      if (array[_i].ANIO === "2014") {
+        this.a12014 = array[_i].A1;
+        this.a2014 = array[_i].A;
+        this.b2014 = array[_i].B;
+        this.c2014 = array[_i].C;
+        this.d2014 = array[_i].D;
+        this.r2014 = array[_i].R;
+      } else if (array[_i].ANIO == "2015") {
+        this.a12015 = array[_i].A1;
+        this.a2015 = array[_i].A;
+        this.b2015 = array[_i].B;
+        this.c2015 = array[_i].C;
+        this.d2015 = array[_i].D;
+        this.r2015 = array[_i].R;
+      } else if (array[_i].ANIO == "2016") {
+        this.a12016 = array[_i].A1;
+        this.a2016 = array[_i].A;
+        this.b2016 = array[_i].B;
+        this.c2016 = array[_i].C;
+        this.d2016 = array[_i].D;
+        this.r2016 = array[_i].R;
+      } else if (array[_i].ANIO == "2017") {
+        this.a12017 = array[_i].A1;
+        this.a2017 = array[_i].A;
+        this.b2017 = array[_i].B;
+        this.c2017 = array[_i].C;
+        this.d2017 = array[_i].D;
+        this.r2017 = array[_i].R;
+      } else if (array[_i].ANIO == "2018") {
+        this.a12018 = array[_i].A1;
+        this.a2018 = array[_i].A;
+        this.b2018 = array[_i].B;
+        this.c2018 = array[_i].C;
+        this.d2018 = array[_i].D;
+        this.r2018 = array[_i].R;
       }
     }
   }
 
-  createChart(){
+  createChart() {
     this.zone.runOutsideAngular(() => {
-    
+
       var chart = am4core.create("barChart", am4charts.XYChart);
 
       // Add data
@@ -112,7 +112,7 @@ export class FacDisC01Component implements OnInit {
         "C": this.c2014,
         "D": this.d2014,
         "R": this.r2014
-      },{
+      }, {
         "year": 2015,
         "A1": this.a12015,
         "A": this.a2015,
@@ -120,7 +120,7 @@ export class FacDisC01Component implements OnInit {
         "C": this.c2015,
         "D": this.d2015,
         "R": this.r2015
-      },{
+      }, {
         "year": 2016,
         "A1": this.a12016,
         "A": this.a2016,
@@ -128,7 +128,7 @@ export class FacDisC01Component implements OnInit {
         "C": this.c2016,
         "D": this.d2016,
         "R": this.r2016
-      },{
+      }, {
         "year": 2017,
         "A1": this.a12017,
         "A": this.a2017,
@@ -136,7 +136,7 @@ export class FacDisC01Component implements OnInit {
         "C": this.c2017,
         "D": this.d2017,
         "R": this.r2017
-      },{
+      }, {
         "year": 2018,
         "A1": this.a12018,
         "A": this.a2018,
@@ -145,7 +145,7 @@ export class FacDisC01Component implements OnInit {
         "D": this.d2018,
         "R": this.r2018
       }];
-      
+
       // Create axes
       var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
       categoryAxis.dataFields.category = "year";
@@ -154,10 +154,10 @@ export class FacDisC01Component implements OnInit {
       categoryAxis.renderer.grid.template.location = 0;
       categoryAxis.renderer.cellStartLocation = 0.1;
       categoryAxis.renderer.cellEndLocation = 0.9;
-      
-      var  valueAxis = chart.xAxes.push(new am4charts.ValueAxis()); 
+
+      var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
       valueAxis.renderer.opposite = true;
-      
+
       // Create series
       function createSeries(field, name) {
         var series = chart.series.push(new am4charts.ColumnSeries());
@@ -167,14 +167,14 @@ export class FacDisC01Component implements OnInit {
         series.columns.template.tooltipText = "{name}: [bold]{valueX}[/]";
         series.columns.template.height = am4core.percent(100);
         series.sequencedInterpolation = true;
-      
+
         var valueLabel = series.bullets.push(new am4charts.LabelBullet());
         valueLabel.label.text = "{valueX}";
         valueLabel.label.horizontalCenter = "left";
         valueLabel.label.dx = 10;
         valueLabel.label.hideOversized = false;
         valueLabel.label.truncate = false;
-      
+
         var categoryLabel = series.bullets.push(new am4charts.LabelBullet());
         categoryLabel.label.text = "{name}";
         categoryLabel.label.horizontalCenter = "right";
@@ -191,7 +191,7 @@ export class FacDisC01Component implements OnInit {
       createSeries("R", "R");
 
       chart.legend = new am4charts.Legend();
-  });
+    });
 
 
   }
--- frontend/src/app/components/distancia/fac-dis-c02/fac-dis-c02.component.ts
+++ frontend/src/app/components/distancia/fac-dis-c02/fac-dis-c02.component.ts
@@ -40,6 +40,7 @@ export class FacDisC02Component implements OnInit {
           .subscribe(res=>{
               this.service.facDisC02Array = res as FacDistanciaC02[];
               this.countData();
+              this.createChart();
           });
   }
 
@@ -114,6 +115,91 @@ export class FacDisC02Component implements OnInit {
             }
           }
       }
+  }
+
+  createChart(){
+    this.zone.runOutsideAngular(() => {
+    
+      var chart = am4core.create("barChart", am4charts.XYChart);
+
+      // Add data
+      chart.data = [{
+        "year": 2014,
+        "Junior": this.junior2014,
+        "Asociado": this.asociado2014,
+        "Senior": this.senior2014,
+        "Emérito": this.emerito2014
+      },{
+        "year": 2015,
+        "Junior": this.junior2015,
+        "Asociado": this.asociado2015,
+        "Senior": this.senior2015,
+        "Emérito": this.emerito2015
+      },{
+        "year": 2016,
+        "Junior": this.junior2016,
+        "Asociado": this.asociado2016,
+        "Senior": this.senior2016,
+        "Emérito": this.emerito2016
+      },{
+        "year": 2017,
+        "Junior": this.junior2017,
+        "Asociado": this.asociado2017,
+        "Senior": this.senior2017,
+        "Emérito": this.emerito2017
+      },{
+        "year": 2018,
+        "Junior": this.junior2018,
+        "Asociado": this.asociado2018,
+        "Senior": this.senior2018,
+        "Emérito": this.emerito2018
+      }];
+      
+      // Create axes
+      var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
+      categoryAxis.dataFields.category = "year";
+      categoryAxis.numberFormatter.numberFormat = "#";
+      categoryAxis.renderer.inversed = true;
+      categoryAxis.renderer.grid.template.location = 0;
+      categoryAxis.renderer.cellStartLocation = 0.1;
+      categoryAxis.renderer.cellEndLocation = 0.9;
+      
+      var  valueAxis = chart.xAxes.push(new am4charts.ValueAxis()); 
+      valueAxis.renderer.opposite = true;
+      
+      // Create series
+      function createSeries(field, name) {
+        var series = chart.series.push(new am4charts.ColumnSeries());
+        series.dataFields.valueX = field;
+        series.dataFields.categoryY = "year";
+        series.name = name;
+        series.columns.template.tooltipText = "{name}: [bold]{valueX}[/]";
+        series.columns.template.height = am4core.percent(100);
+        series.sequencedInterpolation = true;
+      
+        var valueLabel = series.bullets.push(new am4charts.LabelBullet());
+        valueLabel.label.text = "{valueX}";
+        valueLabel.label.horizontalCenter = "left";
+        valueLabel.label.dx = 10;
+        valueLabel.label.hideOversized = false;
+        valueLabel.label.truncate = false;
+      
+        var categoryLabel = series.bullets.push(new am4charts.LabelBullet());
+        categoryLabel.label.text = "{name}";
+        categoryLabel.label.horizontalCenter = "right";
+        categoryLabel.label.dx = -10;
+        categoryLabel.label.fill = am4core.color("#fff");
+        categoryLabel.label.hideOversized = false;
+        categoryLabel.label.truncate = false;
+      }
+      createSeries("Junior", "Junior");
+      createSeries("Asociado", "Asociado");
+      createSeries("Senior", "Senior");
+      createSeries("Emérito", "Emérito");
+      chart.legend = new am4charts.Legend();
+  });
+
+
   }
 
 }
