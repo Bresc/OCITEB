7f0fd800f591d7c77de991b261f07a1427f46f1a
Cristian Guerrero <cristian.guerrero@uptc.edu.co>
2019-10-22 00:13:44 -0500

    i01 edu funcional

--- frontend/src/app/components/educacion/educacion.component.ts
+++ frontend/src/app/components/educacion/educacion.component.ts
@@ -44,6 +44,8 @@ export class EducacionComponent implements OnInit {
             .subscribe(res=>{
                   this.service.facEduI01Array = res as FacEducacionI01[];
                   this.countData();
+                  this.createChart();
+                  this.createGraph();
             });
   }
 
@@ -100,6 +102,133 @@ export class EducacionComponent implements OnInit {
       }
     }
   }
+
+  createChart(){
+    this.zone.runOutsideAngular(() => {
+      let chart = am4core.create("barChart", am4charts.XYChart);
+  
+      // ... chart code goes here ...
+      chart.paddingRight = 20;
+
+      chart.data = [{
+        "Año": "2014",
+        "Num": this.anio2014
+      }, {
+        "Año": "2015",
+        "Num": this.anio2015
+      }, {
+        "Año": "2016",
+        "Num": this.anio2016
+      }, {
+        "Año": "2017",
+        "Num": this.anio2017
+      }, {
+        "Año": "2018",
+        "Num": this.anio2018
+      }];
+
+      let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+      categoryAxis.dataFields.category = "Año";
+      categoryAxis.title.text = "Año";
+
+      let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+      valueAxis.title.text = "Num Proyectos";
+      
+      let series = chart.series.push(new am4charts.ColumnSeries());
+    series.name = "Num";
+    series.columns.template.tooltipText = "Series: {name}\nCategory: {categoryX}\nValue: {valueY}";
+    series.columns.template.fill = am4core.color("#104547"); // fill
+    series.dataFields.valueY = "Num";
+    series.dataFields.categoryX = "Año";
+
+      this.barChart = chart;
+    });
+  }
+
+  createGraph() {
+    this.zone.runOutsideAngular(() => {
+    
+      let chart = am4core.create("chartdiv", am4charts.XYChart);
+
+      // Add data
+      chart.data = [{
+        "year": "2014",
+        "Capital Semilla": this.semilla2014,
+        "Contrapartida": this.contra2014,
+        "Sin Financiación": this.sinfin2014
+      }, {
+        "year": "2015",
+        "Capital Semilla": this.semilla2015,
+        "Contrapartida": this.contra2015,
+        "Sin Financiación": this.sinfin2015
+      }, {
+        "year": "2016",
+        "Capital Semilla": this.semilla2016,
+        "Contrapartida": this.contra2016,
+        "Sin Financiación": this.sinfin2016
+      }, {
+        "year": "2017",
+        "Capital Semilla": this.semilla2017,
+        "Contrapartida": this.contra2017,
+        "Sin Financiación": this.sinfin2017
+      }, {
+        "year": "2018",
+        "Capital Semilla": this.semilla2018,
+        "Contrapartida": this.contra2018,
+        "Sin Financiación": this.sinfin2018
+      }];
+
+      // Create category axis
+      let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+      categoryAxis.dataFields.category = "year";
+      categoryAxis.renderer.opposite = false;
+
+      // Create value axis
+      let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+      valueAxis.renderer.inversed = false;
+      valueAxis.title.text = "Place taken";
+      valueAxis.renderer.minLabelPosition = 0.01;
+
+      // Create series
+      let series1 = chart.series.push(new am4charts.LineSeries());
+      series1.dataFields.valueY = "Capital Semilla";
+      series1.dataFields.categoryX = "year";
+      series1.name = "Capital Semilla";
+      series1.strokeWidth = 3;
+      series1.bullets.push(new am4charts.CircleBullet());
+      series1.tooltipText = "{name} in {categoryX}: {valueY}";
+      series1.legendSettings.valueText = "{valueY}";
+      series1.visible = false;
+
+      let series2 = chart.series.push(new am4charts.LineSeries());
+      series2.dataFields.valueY = "Contrapartida";
+      series2.dataFields.categoryX = "year";
+      series2.name = 'Contrapartida';
+      series2.strokeWidth = 3;
+      series2.bullets.push(new am4charts.CircleBullet());
+      series2.tooltipText = "{name} in {categoryX}: {valueY}";
+      series2.legendSettings.valueText = "{valueY}";
+
+      let series3 = chart.series.push(new am4charts.LineSeries());
+      series3.dataFields.valueY = "Sin Financiación";
+      series3.dataFields.categoryX = "year";
+      series3.name = 'Sin Financiación';
+      series3.strokeWidth = 3;
+      series3.bullets.push(new am4charts.CircleBullet());
+      series3.tooltipText = "{name} in {categoryX}: {valueY}";
+      series3.legendSettings.valueText = "{valueY}";
+
+      // Add chart cursor
+      chart.cursor = new am4charts.XYCursor();
+      chart.cursor.behavior = "zoomY";
+
+      // Add legend
+      chart.legend = new am4charts.Legend();
+      this.barChart = chart;
+    });
+   
+  }
+
 
   ngOnDestroy() {
     this.zone.runOutsideAngular(() => {
